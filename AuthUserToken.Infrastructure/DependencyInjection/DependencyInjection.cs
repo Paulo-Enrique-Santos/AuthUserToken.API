f66f64a06e80177cd62b222a97c038b4d95cb9d1
Paulo Filho <paulo.filho@genial.com.vc>
2023-03-17 10:52:40 -0300

    feat: create DbContext

---
+++ AuthUserToken.Infrastructure/DependencyInjection/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using AuthUserToken.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AuthUserToken.Infrastructure.DependencyInjection
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlite(configuration.GetConnectionString("ConnectionSqlite")));
+
+            return services;
+        }
+
+        public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
+    }
+}
